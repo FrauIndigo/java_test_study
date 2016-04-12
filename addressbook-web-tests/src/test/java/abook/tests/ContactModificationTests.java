2b33c54467af5375503d1dc1bc7ded44639fb0bb
FrauIndigo <irina_ru@mail.ru>
2016-04-12 23:39:13 +0300

    Проверка и обеспечение выполнения предусловий
    для групп

--- addressbook-web-tests/src/test/java/abook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/abook/tests/ContactModificationTests.java
@@ -10,6 +10,8 @@ public class ContactModificationTests extends TestBase{
     @Test
     public void testContactModification() {
         app.getNavigationHelper().gotoContactsList();
+
+
         app.getContactHelper().selectContact();
         app.getContactHelper().modifySelectedContact();
         app.getContactHelper().fillContactInfo(new ContactData("Second", "M", "Last", "test1", "test2", "test3", "test4", "test5", "first.last@fm.com", null),false);
--- addressbook-web-tests/src/test/java/abook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/abook/tests/GroupDeletionTests.java
@@ -10,6 +10,9 @@ public class GroupDeletionTests extends TestBase{
     @Test
     public void testGroupDeletion(){
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroup();
         app.getNavigationHelper().gotoContactsList();
